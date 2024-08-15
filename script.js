a6d9c17eb8624b311eeb5a416296a3ce81df8f1c
aironlazza <aironlazza@gmail.com>
2024-08-15 11:49:39 -0300

    css

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("tela");
+let ctx = canvas.getContext("2d");
+// canvas.width = width;
+// canvas.height = height;
+ctx.fillStyle = "rgb(0,255,0)"
+ctx.fillRect(0,0,2000,2000)
